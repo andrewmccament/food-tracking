87f1b3fb57df078fed921d130d5604e623deabc2
Andrew McCament <andrew@mccament.net>
2026-09-19 11:56:20 -0500

    remove save/discard pop-up (intrusive)

--- app/(log)/log.tsx
+++ app/(log)/log.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { View, StyleSheet, Alert, Keyboard } from "react-native";
-import MealSummary from "@/components/Shared/MealSummary";
-import { useDispatch, useSelector } from "react-redux";
+import { View, StyleSheet, Keyboard } from "react-native";
+import { useDispatch } from "react-redux";
 import { logMeal } from "@/state/foodSlice";
 import { Chat } from "@/components/Log/Chat";
-import { router, useLocalSearchParams, useNavigation } from "expo-router";
-import { RootState } from "@/state/store";
+import { useLocalSearchParams, useNavigation } from "expo-router";
 
 export default function LoggingScreen() {
   const { logMode } = useLocalSearchParams();
@@ -18,39 +16,22 @@ export default function LoggingScreen() {
   }, [logMode, navigation]);
 
   const mealId = React.useRef<string | undefined>(undefined);
-  const [mealIdChanged, setMealIdChanged] = React.useState<string>();
-  const userAddedMeal = React.useRef(false);
   const dispatch = useDispatch();
-  const meals = useSelector((state: RootState) => state.food.meals);
+
   React.useEffect(() => {
     return () => {
       Keyboard.dismiss();
-      setTimeout(() => {
-        if (
-          mealId.current &&
-          !meals.find((meal) => meal.mealId === mealId.current)?.isAdded
-        ) {
-          const unsavedMealId = mealId.current;
-          if (!unsavedMealId) return;
-          Alert.alert("Did you want to save the meal or discard?", "", [
-            {
-              text: "Discard",
-              onPress: () => {},
-              style: "cancel",
-            },
-            { text: "Save", onPress: () => dispatch(logMeal(unsavedMealId)) },
-          ]);
-        }
-      }, 1000);
+      if (mealId.current) {
+        dispatch(logMeal(mealId.current));
+      }
     };
-  }, []);
+  }, [dispatch]);
 
   return (
     <View style={styles.container}>
       <Chat
         onMealRetrieval={(thisMealId) => {
           mealId.current = thisMealId;
-          setMealIdChanged(thisMealId);
         }}
       />
     </View>
